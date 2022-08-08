09b3cc579cdd7bc8551dd53f19578a9968a68255
Lorenzo <caramaschilolo@gmail.com>
2022-08-08 16:22:22 -0300

    Portfolio 0.0.2

---
+++ js/proyectos.js
@@ -0,0 +1,38 @@
+let proyectos = [
+    {
+        imagen: "./img/Tarjetas Proyectos/GagEnGug.png",
+        titulo: "Gag en Gug",
+        texto: "Sitio web creado para compartir textos. Podés enviarlos a través de un formulario para que luego sean subidos a la plataforma. El diseño de toda la página lo hice con Adobe Illustrator y utilicé <span class='tag'>HTML, CSS y SASS</span>.",
+        repositorio: "https://github.com/lorenzocaramaschi/gag-en-gug",
+        link: "https://gagengug.netlify.app/index.html"
+    },
+    {
+        imagen: "./img/Tarjetas Proyectos/TorneosLBDL.png",
+        titulo: "Torneos de La Banda del Lobo",
+        texto: "Sitio web para una organización de torneos de FIFA (E-sports) durante el curso de Desarrollo Web en Coderhouse. La misma fue realizada con <span class='tag'>HTML, CSS, SASS y BootStrap</span>.",
+        repositorio: "https://github.com/lorenzocaramaschi/Desarrollo-Web---Torneos-LBDL",
+        link: "https://torneoslbdl.netlify.app/index.html"
+    },
+    {
+        imagen: "./img/Tarjetas Proyectos/Glen.png",
+        titulo: "Glenda Camarón",
+        texto: "Landing page para 'Glendacamarón', artista urbana. Para hacer la pagina utilicé <span class='tag'>HTML y CSS</span>.",
+        repositorio: "https://github.com/lorenzocaramaschi/GlendaCamaron-Portfolio",
+        link: "https://glenda-camaron.netlify.app"
+    },
+    {
+        imagen: "./img/Tarjetas Proyectos/Tobo.png",
+        titulo: "Tobías Índigo",
+        texto: "Landing page para 'Tobías Indigo', cantate y productor musical. En ella podes ver sus últimas novedades y hasta escuchar sus canciones a través de Spotify. La página fue realizada con <span class='tag'>HTML y CSS</span>.",
+        repositorio: "https://lorenzocaramaschi.github.io/Tobias-Indigo-Portfolio/",
+        link: "https://tobias-indigo.netlify.app"
+    },
+    {
+        imagen: "./img/Tarjetas Proyectos/BuscarPro.png",
+        titulo: "Buscar Pro",
+        texto: "E-Commerce realizado durante el curso de 'Javascript' de Coderhouse. Para realizarlo utilicé <span class='tag'>HTML, CSS, SASS, BootStrap y Javascript</span>.",
+        repositorio: "https://github.com/lorenzocaramaschi/gag-en-gug",
+        link: "https://buscarpro.netlify.app"
+    },
+
+]
